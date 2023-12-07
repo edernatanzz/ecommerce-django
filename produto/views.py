cbd322463fa8088e540ed2e96dd4e26b8337260e
EderNatan.dev <edernatan124@gmail.com>
2023-12-07 00:15:30 -0300

    v 0.7

--- produto/views.py
+++ produto/views.py
@@ -2,13 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
+from . import models 
+
 # Create your views here.
 
 
-class ListaProdutos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('listarprodutos')
-
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name='produtos'
+    paginate_by= 10
+    
 class DetalheProduto(View):
     def get(self, *args, **kwargs):
         return HttpResponse('detalheproduto')
