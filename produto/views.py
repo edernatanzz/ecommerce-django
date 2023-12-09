261e14b2290b9192720f70287fcf5474357cb0e2
EderNatan.dev <edernatan124@gmail.com>
2023-12-09 20:15:57 -0300

    v 0.8

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models 
 
@@ -11,11 +12,13 @@ class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
     context_object_name='produtos'
-    paginate_by= 10
+    paginate_by= 3
     
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('detalheproduto')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name='produto'
+    slug_url_kwarg = 'slug'
 
 class AdicionarCarrinho(View):
     def get(self, *args, **kwargs):
